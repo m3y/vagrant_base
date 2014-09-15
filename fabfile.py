db474a2db1e99d5d483360318d2d6ee25841fff3
m3y <ma3ya.ozw+github@gmail.com>
2014-09-15 23:05:45 +0900

    新規作成

---
+++ fabfile.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from fabric.api import run
+from fabric.api import cd
+from fabric.api import sudo
+from fabric.api import env
+from fabric.colors import red, green
+
+import cuisine
+
+cuisine.select_package("apt")
+
+
+def setup():
+    _setup_ubuntu()
+    _install_dotfiles()
+
+
+def _setup_ubuntu():
+    sudo("cp /usr/share/zoneinfo/Japan /etc/localtime")
+    sudo("apt-get update")
+    cuisine.package_ensure('git')
+    cuisine.package_ensure('exuberant-ctags')
+
+
+def _install_dotfiles():
+    env.forward_agent = True
+    run("git clone git@github.com:m3y/dotfiles.git")
+    with cd("/home/vagrant/dotfiles"):
+        run("make install")
+    print(green("[dotfiles] installed."))
